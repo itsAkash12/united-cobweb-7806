a8e654b64867c50e8b5d10200467517a1542684e
itsAkash12 <akashviratsingh@gmail.com>
2022-11-10 04:39:20 +0530

    added:day-2 tasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Header/Navbar';
+import SmallNavbar from './components/Header/SmallNavbar';
+import TopSection from './components/HomeSection/TopSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <TopSection />
     </div>
   );
 }
