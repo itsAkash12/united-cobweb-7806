f49cabecf31c3a5f18f3cfd66613f96e29d1c645
Vishal <vishaldas6566@gmail.com>
2022-11-11 03:49:19 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,19 @@ import TopSection from './components/HomeSection/TopSection';
 import "./App.css";
 import GiftCards from "./components/GiftCards/GiftCards";
 import Products from './pages/Products';
-
+import AcerProductPage from './pages/ProductPages/AcerProductPage';
+import AmazonProductPage from './pages/ProductPages/AmazonProductPage';
+import AndroidProductPage from './pages/ProductPages/AndroidProductPage';
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <TopSection />
       <GiftCards /> */}
-  <Products />
+  {/* <Products /> */}
+  {/* <AcerProductPage /> */}
+  {/* <AmazonProductPage /> */}
+  <AndroidProductPage />
     </div>
   );
 }
