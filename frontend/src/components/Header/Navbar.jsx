9179af7a57713703fc2dc45391eb4ea205c2bc41
raobaba <shrikishunr7@gmail.com>
2022-11-12 03:50:28 +0530

    commit changes

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -20,7 +20,7 @@ import {
 } from "@chakra-ui/react";
 import { ChevronDownIcon,SearchIcon } from "@chakra-ui/icons";
 import SmallNavbar from "./SmallNavbar";
-import Link from "react-router-dom"
+import {Link} from "react-router-dom"
 function Navbar() {
   const [city, setCity] = useState("");
   let key = "dae6521ae39fc64f8d201f17c57efeb5";
@@ -102,7 +102,7 @@ function Navbar() {
                     color="white"
                     _hover={{ bg: "white", color: "#0046BE" }}
                   >
-                    <Link>Sign In</Link>
+                    <Link to="/login">Sign In</Link>
                   </Button>
                 </Box>
               </MenuItem>
