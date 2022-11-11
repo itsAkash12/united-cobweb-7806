44d7641932fe7f4aad8d6b5c639ad5a7ebf79f8e
Vishal <vishaldas6566@gmail.com>
2022-11-11 15:55:24 +0530

    added

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,15 +1,109 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import ProductBanner from '../components/Products/ProductBanner'
 import ProductTopCrousal from '../components/Products/ProductTopCrousal'
+import AllProductPageRoute from './ProductPages/AllProductPageRoute';
+
 function Products() {
+  const ProductData = [
+    {
+      id: 1,
+      imgUrl:
+        "https://pbs.twimg.com/profile_images/915251508760920065/CS3k9D2W_400x400.jpg",
+      title: "Acer Products",
+      rate: "⭐⭐⭐⭐ (293 Reviews)",
+      link:"/acerproductpage"
+      
+    },
+    {
+      id: 2,
+      imgUrl:
+        "https://cdn.vox-cdn.com/thumbor/o3hWfI8VxYP_EoOVqHYw0mYjTfE=/0x0:792x528/1400x1400/filters:focal(396x264:397x265)/cdn.vox-cdn.com/uploads/chorus_asset/file/19232117/amazonlineup.jpg",
+      title:
+        "Amazon Products",
+      rate: "⭐⭐⭐⭐ (152 Reviews)",
+      link:"/amazonproductpage"
+    },
+    {
+      id: 3,
+      imgUrl:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/android-phones-1651081480.jpg?crop=0.554xw:0.739xh;0.0465xw,0.141xh&resize=640:*",
+      title:
+        "Android Products",
+      rate: "⭐⭐⭐⭐ (420 Reviews)",
+      link:"/androidproductpage"
+    },
+    {
+      id: 4,
+      imgUrl:
+        "https://multimedia.bbycastatic.ca/multimedia/products/250x250/160/16003/16003193.jpg",
+      title: "Iphone Products",
+      rate: "⭐⭐⭐⭐ (212 Reviews)",
+      style:"mt-24",
+      link:"/iphoneproductpage"
+    },
+    {
+      id: 5,
+      imgUrl:
+        "https://i2-prod.mirror.co.uk/incoming/article20823613.ece/ALTERNATES/s1200c/0_Amazon-Echo-devices.jpg",
+      title: "Latest Product",
+      rate: "⭐⭐⭐⭐ (56 Reviews)",
+ style:"mt-4",
+ link:"/latestproductpage"
+    },
+    {
+      id: 6,
+      imgUrl:
+        "https://image.shutterstock.com/shutterstock/photos/1248412693/display_1500/stock-photo-gadgets-and-accessories-isolated-on-white-background-1248412693.jpg",
+      title:
+        "MarketPlace Products",
+      rate: "⭐⭐⭐⭐ (51 Reviews)",
+      style:"mt-20",
+      link:"/marketplaceproductpage"
+   
+    },
+    {
+      id: 7,
+      imgUrl:
+        "https://newlintech.com/wp-content/uploads/2020/05/Best-Smart-Home-Devices-2018.jpg",
+      title:
+        "Smarthome Products",
+      rate: "⭐⭐⭐⭐ (89 Reviews)",
+      style:"mt-48",
+      link:"/smarthomeproductpage"
+     
+    },
+    {
+      id: 8,
+      imgUrl:
+        "https://thumbs.dreamstime.com/b/sponsor-electronic-gadgets-office-supplies-collection-214012719.jpg",
+      title:
+        "Sponsered Products",
+      rate: "⭐⭐⭐⭐ (1 Reviews)",
+      style:"mt-32",
+      link:"/sponseredproductpage"
+    },
+
+    {
+      id: 9,
+      imgUrl:
+        "https://i.gadgets360cdn.com/large/flipkart_gadgets_sale_1521539020287.jpg",
+      title: "Top Deals",
+      rate: "⭐⭐⭐⭐ (1 Reviews)",
+      style:"mt-20",
+      link:"/topdealsproductpage"
+    }
+   
+  ];
   return (
     <div >
+      <AllProductPageRoute />
  <ProductBanner />
    <ProductTopCrousal />
    <div className='border-solid border-b-2 border-gray-200'>
    <p className='text-3xl w-fit mt-4 ml-8 py-2 '>Products</p>
    </div>
-   <div className='mt-8 ' style={{ 
+   <div className='mt-8 w-fit ' style={{ 
       backgroundImage: `url(" https://pisces.bbystatic.com/image2/BestBuy_US/dam/custom_fluid_xl_mmt-599505-715e51c3-2718-4a87-a9b7-1dd78586d130.jpg")` ,
       backgroundRepeat: 'no-repeat',
     
@@ -36,64 +130,24 @@ function Products() {
        </div>
    </div>
 
-{/* <div className='grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-10'>
-  <div className='border-2 p-3'>
-    <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-config-201810?wid=1078&hei=624&fmt=jpeg&qlt=90&.v=1664499515473" alt="" />
-  <p className='text-md  text-sky-500 cursor-pointer hover:underline my-2 '>MacBook <br />
-Save up to $200 on select models.</p>
-<p className='text-sm text-gray-400'>Minimum savings is $50.</p>
-<button className='bg-blue-600 text-white rounded-md py-1 mt-9 text-sm px-8'>Shop Now</button>
+   <div className='grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4  mt-10'>
+  {
+    ProductData.map((el)=>(
+<div className=' border-2 p-3' key={el.id}>
+    <img src={el.imgUrl} alt="" />
+   
+    <p className={`text-2xl font-bold  text-sky-500 cursor-pointer hover:underline my-2 ${el.style}`} >{el.title}</p>
+  
+   <p className='text-sm text-gray-400 '>Get all {el.title}</p>
+   <Link to={el.link}> <button className='bg-blue-900 text-white rounded-md py-1 mt-6 text-sm px-4 w-fit'>Shop Now</button></Link>
+  </div>
+    ))
+  }
+  
+  
+    
   </div>
 
-
-  <div className='border-2 p-3'>
-    <img src="https://image.cnbcfm.com/api/v1/image/107136506-ipad-2022.PNG?v=1666616401&w=929&h=523" alt="" />
-  <p className='text-md  text-sky-500 cursor-pointer hover:underline my-2 '>Lovable. Drawable. Magical.</p>
-<p className='text-sm text-gray-400'>Accessories sold separately.</p>
-<button className='bg-blue-600 text-white rounded-md py-1 mt-16 text-sm px-8'>Shop Now</button>
-  </div>
-
-
-  <div className='border-2 p-3'>
-    <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-11-og-202210?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1664504610941" alt="" />
-  <p className='text-md  text-sky-500 cursor-pointer hover:underline my-2 '>iPad Pro</p>
-<p className='text-sm text-gray-400'>Supercharged by M2.</p>
-<button className='bg-blue-600 text-white rounded-md py-1 mt-20 text-sm px-8'>Shop Now</button>
-  </div>
-
-  <div className='border-2 p-3'>
-    <img src="https://www.apple.com/newsroom/images/product/iphone/geo/Apple-iPhone-14-iPhone-14-Plus-hero-220907-geo_Full-Bleed-Image.jpg.large.jpg" alt="" />
-  <p className='text-md  text-sky-500 cursor-pointer hover:underline my-2 '>iPhone 14 and iPhone 14 Plus <br />
-Big and bigger.</p>
-<p className='text-sm text-gray-400'>Save up to $800 with qualified activation and <br />
- trade-in of  iPhone 12 or newer.</p>
-<button className='bg-blue-600 text-white rounded-md py-1 mt-6 text-sm px-8'>Shop Now</button>
-  </div>
-
-  <div className='border-2 p-3'>
-    <img src="https://www.apple.com/v/iphone-14-pro/c/images/overview/hero/hero_endframe__dtzvajyextyu_large.jpg" alt="" />
-  <p className='text-md  text-sky-500 cursor-pointer hover:underline my-2 '>iPhone 14 Pro <br />
-
-Pro. Beyond.</p>
-
-<button className='bg-blue-600 text-white rounded-md py-1 mt-8 text-sm px-8'>Shop Now</button>
-  </div>
-
-  <div className='border-2 p-3'>
-    <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/homepod-mini-og-202110?wid=600&hei=315&fmt=jpeg&qlt=95&.v=1632930650000" alt="" />
-  <p className='text-md  text-sky-500 cursor-pointer hover:underline my-2 mt-20 '>Color your world with HomePod mini.
-Shop Now</p>
-
-<button className='bg-blue-600 text-white rounded-md py-1 mt-10 text-sm px-8'>Shop Now</button>
-  </div>
-
-  <div className='border-2 p-3'>
-    <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-hero-select-202011_FMT_WHH?wid=607&hei=556&fmt=jpeg&qlt=90&.v=1633623988000" alt="" className='h-3/5' />
-  <p className='text-md  text-sky-500 cursor-pointer hover:underline my-2 '>Apple - AirPods Max - Space Gray</p>
-
-<button className='bg-blue-600 text-white rounded-md py-1 mt-10 text-sm px-8'>Shop Now</button>
-  </div>
-</div> */}
 
    {/* <div style={{ 
       backgroundImage: `url("https://pisces.bbystatic.com/image2/BestBuy_US/dam/gl-77114-giftideas-ep-td-half_der-e5653191-ad0b-42de-a484-6759bfdd6ad5.jpg")` ,
