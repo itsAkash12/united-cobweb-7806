e021c22132a79faff91261e136cdeac2f66c3e88
itsAkash12 <akashviratsingh@gmail.com>
2022-11-11 08:30:20 +0530

    added:day-3 tasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import SmallNavbar from './components/Header/SmallNavbar';
 import TopSection from './components/HomeSection/TopSection';
 import "./App.css";
 import GiftCards from "./components/GiftCards/GiftCards";
+import MiddleSection from './components/HomeSection/MiddleSection';
+import SliderCard from './components/Slider/SliderCard';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <TopSection />
+      <MiddleSection />
       <GiftCards />
+      {/* <SliderCard /> */}
     </div>
   );
 }
