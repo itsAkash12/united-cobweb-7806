be1e2411838b3ab4874396bfc54d658d46e9027c
Vishal <vishaldas6566@gmail.com>
2022-11-12 15:51:28 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,13 @@
 import "./App.css";
-import Navbar from "./components/Header/Navbar";
-import SmallNavbar from "./components/Header/SmallNavbar";
-import TopSection from "./components/HomeSection/TopSection";
-import GiftCards from "./components/GiftCards/GiftCards";
-import MiddleSection from "./components/HomeSection/MiddleSection";
-import SliderCard from "./components/Slider/SliderCard";
-import Products from "./pages/Products";
-import AcerProductPage from "./pages/ProductPages/AcerProductPage";
-import AmazonProductPage from "./pages/ProductPages/AmazonProductPage";
-import AndroidProductPage from "./pages/ProductPages/AndroidProductPage";
-import IphoneProductPage from "./pages/ProductPages/IphoneProductPage";
-import LatestProductPage from "./pages/ProductPages/LatestProductPage";
-import MarketPlaceProductPage from "./pages/ProductPages/MarketplaceProducrPage";
-import SmarthomeProductPage from "./pages/ProductPages/SmarthomeProductPage";
-import SponseredProductPage from "./pages/ProductPages/SponseredProductPage";
-import TopdealsProductPage from "./pages/ProductPages/TopdealsProductPage";
-import SliderCarousel from "./components/Slider/SliderCarousel";
-import BottomSection from "./components/HomeSection/BottomSection";
-import Homepage from "./pages/Homepage/Homepage";
+
+
 import AllRoutes from "./routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
       <AllRoutes />
-      {/* <Products /> */}
+     
     </div>
   );
 }
