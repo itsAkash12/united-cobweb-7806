b9eda8793672382efcb5db051685d44725e810dd
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-04-03 14:00:15 +0530

    Backend redeploy

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,6 +1,6 @@
 // export const api = "https://bestbuy-backend.onrender.com/";
 
-export const user_api = "https://techhubbackend-production.up.railway.app/user";
+export const user_api = "https://comfortable-tick-necklace.cyclic.app/user";
 
 export const product_api =
-  "https://techhubbackend-production.up.railway.app/product";
+  "https://comfortable-tick-necklace.cyclic.app/product";
